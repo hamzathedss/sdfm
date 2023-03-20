06147ae0510d8f029cff7c709ecab78dc3699462
hamzathedss <hamzathedss@gmail.com>
2023-03-20 04:40:43 +0000

    mmm

--- src/components/NewsFooter.js
+++ src/components/NewsFooter.js
@@ -1,14 +1,37 @@
 import Ticker, {NewsTicker} from 'nice-react-ticker';
-import {useId} from "react";
+import {useId, useState} from "react";
 import {useQuery} from "react-query";
 import {noteService} from "../_services";
 import {ThreeDots} from "react-loader-spinner";
+import Pusher from "pusher-js";
+import NotificationSound from "../assets/notification_sound.wav";
 
 const NewsFooter = () => {
     const randomID = useId();
 
     const {isLoading, data} = useQuery(randomID, () => noteService.getNoteData());
     const noteData = data || {"data": []}
+
+    const [noteId, setNoteId] = useState(0);
+    const [noteName, setNoteName] = useState('');
+    const [noteDescription, setNoteDescription] = useState('');
+
+    const audio = new Audio(NotificationSound);
+
+    Pusher.logToConsole = true;
+
+    let pusher = new Pusher('5fdf984542cc12f08fb0', {
+        cluster: 'eu'
+    });
+
+    let channel = pusher.subscribe('sdfm-channel');
+
+    channel.bind('sdfm-note', function (data) {
+        setNoteId(data.id);
+        setNoteName(data.name);
+        setNoteDescription(data.description);
+        audio.play();
+    });
 
     if (isLoading) {
         return (
@@ -29,14 +52,9 @@ const NewsFooter = () => {
 
     return (
         <Ticker isNewsTicker={true} slideSpeed="20">
-            {
-                Object.values(noteData).map(ele =>
-                    <NewsTicker key={ele.id} id={ele.id}
-                                title={ele.description}
-                                meta={ele.name} url=""/>
-                )
-
-            }
+            <NewsTicker id={noteId || noteData[0].id}
+                        title={noteDescription || noteData[0].description}
+                        meta={noteName || noteData[0].name} url=""/>
         </Ticker>
     );
 }
